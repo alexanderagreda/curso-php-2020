0997061f8738d8244927d5c5e76c1af188bcb71f
Alexander <alexanderagreda@gmail.com>
2020-04-16 21:30:01 -0500

    validate form and finish

--- formulario-contacto/index.php
+++ formulario-contacto/index.php
@@ -1,5 +1,66 @@
 <?php
-    
+    //Variable declarada y utilizada para mostrar los errores de entrada de datos
+    $error = '';
+    $enviado = "";
+
+    //Validar si se establecio la varible submit con el metodo post
+    if (isset($_POST['submit'])) {
+        //recuperar los datos y asignarlos a las variables
+        $nombre = $_POST['nombre'];
+        $correo = $_POST['correo'];
+        $mensaje = $_POST['mensaje'];
+
+        // Validar si hay contenido en la variable nombre (no esta vacia)
+        if (!empty($nombre)) {
+            //Limpiar la variable de caracteres no deseados
+            $nombre = trim($nombre);
+            $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+
+        }else{
+            $error .= 'Por favor ingrese un nombre <br />';
+        } 
+        
+        // Validar si hay contenido en la variable correo (no esta vacia)
+        if (!empty($correo)) {
+            $correo = trim($correo);
+            $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+            
+            //Vefificar si el conteido de la variable correo es válido 
+            if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+                $error .= 'Por favor escriba un correo válido <br />';
+            }
+
+        }else{
+            $error .=  'Por favor escriba una dirección de correo <br />';
+        }
+
+
+        // Validar si hay contenido en la variable mensaje (no esta vacia)
+        if (!empty($mensaje)) {
+            $mensaje = htmlspecialchars($mensaje);
+            $mensaje = trim($mensaje);
+            $mensaje = stripcslashes($mensaje);
+        }else{
+            $error .= 'Por favor escribe un mensaje';
+        }
+
+        //Enviar mensaje
+        if (!$error) {
+            $destinatario = 'alexanderagreda@gmail.com';
+            $asunto = 'Mensaje enviado desde mi formulario de contacto';
+            $contenido = "De: $nombre \n";
+            $contenido .= "Correo electronico: $correo \n";
+            $contenido .= "Mensaje: " . $mensaje;
+            //funcion para enviar el mensaje
+            //mail($destinatario, $asunto, $mensaje);
+            //Confirmar envio de mensaje con la variable enviado
+            $enviado = 'Mensaje eviado correctamente';
+        }
+
+        
+
+
+    }
     require 'index.view.php';
    
 ?>
--- formulario-contacto/index.view.php
+++ formulario-contacto/index.view.php
@@ -13,10 +13,19 @@
         <div class="wrap">
             <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
                 <h1>Formulario de Contacto</h1>
-                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre" value="">
-                <input type="mail" class="form-control" name="correo" id="correo" placeholder="Correo" value="">
-                <textarea name="mensaje" class="form-control" placeholder="Mensaje"></textarea>
+                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre" value="<?php if(!$enviado && isset($nombre)) echo $nombre ?>">
+                <input type="email" class="form-control" name="correo" id="correo" placeholder="Correo" value="<?php if(!$enviado && isset($correo)) echo $correo ?>">
+                <textarea name="mensaje" class="form-control" placeholder="Mensaje"><?php if(!$enviado && isset($mensaje)) echo $mensaje ?></textarea>
                 <!-- here insert error -->
+                <?php if (!empty($error)):  ?>
+                    <div class="alert error">
+                        <?php echo $error; ?>
+                    </div>
+                <?php elseif (!empty($enviado)): ?>
+                    <div class="alert success">
+                        <p>Enviado correctamente</p>
+                    </div>
+                <?php endif; ?>
                 <input type="submit" name="submit" value="Enviar correo" class="btn btn-primary">
             </form>
         </div>   
